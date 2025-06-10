45fa51be307963501a1646347d07e8adf2aba643
abhinandan-debug <abhinandanmishra2005@gmail.com>
2025-06-10 21:33:35 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -7,7 +7,7 @@ async function loadPhotographerProfile() {
     const id = getPhotographerIdFromUrl();
 
    try {
-const response = await fetch("https://run.mocky.io/v3/cdd6d821-6fb7-43c3-a834-ec3f6534ea51/photographers");
+const response = await fetch("https://run.mocky.io/v3/cdd6d821-6fb7-43c3-a834-ec3f6534ea51");
 const data = await response.json();
 const photographer = data.find(p => p.id === id);
 
